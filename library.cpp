3666b38d4f2f399f1e6a44ab96269d9f6d5aad86
hanteo <hanwei.teo@gmail.com>
2017-03-13 12:01:05 +0000

    permutations

--- library.cpp
+++ library.cpp
@@ -1,6 +1,9 @@
 #include "library.h"
 
 #include <iostream>
+#include <algorithm>
+
+using namespace std;
 
 int choice(int n, int k) {
     if (k > n) {
@@ -17,3 +20,40 @@ int choice(int n, int k) {
     return r;
 }
 
+auto combinations(vector<int> A, int k) {
+    auto N = A.size();
+    vector <vector<int>> combinations;
+    string select(k, 1);
+    select.resize(N, 0);
+
+    do {
+        vector<int> c;
+        for (auto i = 0; i < N; i++) {
+            if (select[i]) {
+                c.push_back(A[i]);
+            }
+        }
+        combinations.push_back(c);
+    } while (prev_permutation(select.begin(), select.end()));
+
+    return combinations;
+}
+
+auto kcombinations(vector <vector<int>> A, int k) {
+
+    auto seconds = vector < vector < int >> ();
+
+    for (auto c : A) {
+
+        auto second = combinations(c, k);
+
+        for (auto d: second) {
+            vector<int> combined;
+            combined.insert(combined.end(), c.begin(), c.end());
+            combined.insert(combined.end(), d.begin(), d.end());
+            seconds.push_back(combined);
+        }
+    }
+
+    return seconds;
+}
--- main.cpp
+++ main.cpp
@@ -3,6 +3,51 @@
 #include "catch.hpp"
 #include "library.cpp"
 
-TEST_CASE("7 choose 4 and 4 choose 3 is 140 bit") {
+TEST_CASE("7 choose 4 and 4 choose 3 is 140") {
     REQUIRE(choice(7, 4) * choice(4, 3) == 140);
+}
+
+SCENARIO("7 choose 4 produces 35 combinations") {
+
+    GIVEN("I have seven unique numbers") {
+        vector<int> numbers = {1, 2, 3, 4, 5, 6, 7};
+
+        WHEN("I choose 4 from the numbers") {
+            auto combination = combinations(numbers, 4);
+
+            THEN("I get 35 combinations") {
+                REQUIRE(combination.size() == 35);
+            }
+        }
+    }
+}
+
+SCENARIO("4 choose 3 produces 4 combinations") {
+
+    GIVEN("I have seven unique numbers") {
+        vector<int> numbers = {1, 2, 3, 4};
+
+        WHEN("I choose 4 from the numbers") {
+            auto combination = combinations(numbers, 3);
+
+            THEN("I get 35 combinations") {
+                REQUIRE(combination.size() == 4);
+            }
+        }
+    }
+}
+
+SCENARIO("7 choose 4 then 4 choose 3") {
+
+    GIVEN("I have seven unique numbers") {
+        vector<int> numbers = {1, 2, 3, 4, 5, 6, 7};
+
+        WHEN("I choose 4 from 7 then 3 from 4 numbers") {
+            auto combination = kcombinations(combinations(numbers, 4), 3);
+
+            THEN("I have 140 combinations") {
+                REQUIRE(combination.size() == 140);
+            }
+        }
+    }
 }
