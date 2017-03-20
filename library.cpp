c9ef987ecbe16e736ebf8b7c328ef284fca74fc5
hanteo <hanwei.teo@gmail.com>
2017-03-20 02:24:57 +0000

    permutations

--- library.cpp
+++ library.cpp
@@ -1,5 +1,5 @@
 #include "library.h"
-
+#include "Eigen/Dense"
 #include <iostream>
 #include <algorithm>
 
@@ -20,14 +20,15 @@ int choice(int n, int k) {
     return r;
 }
 
-auto combinations(vector<int> A, int k) {
+template<typename T>
+auto combinations(vector <T> A, int k) {
     auto N = A.size();
-    vector <vector<int>> combinations;
+    vector <vector<T>> combinations;
     string select(k, 1);
     select.resize(N, 0);
 
     do {
-        vector<int> c;
+        vector <T> c;
         for (auto i = 0; i < N; i++) {
             if (select[i]) {
                 c.push_back(A[i]);
@@ -39,13 +40,14 @@ auto combinations(vector<int> A, int k) {
     return combinations;
 }
 
-auto kcombinations(vector <vector<int>> A, int k) {
+template<typename T>
+auto kcombinations(vector <vector<T>> A, int k) {
 
-    vector <vector<int>> seconds;
+    vector <vector<T>> seconds;
     for (auto a : A) {
 
-        for (auto b: combinations(a, k)) {
-            vector<int> combined;
+        for (auto b : combinations(a, k)) {
+            vector <T> combined;
             combined.insert(combined.end(), a.begin(), a.end());
             combined.insert(combined.end(), b.begin(), b.end());
             seconds.push_back(combined);
@@ -53,4 +55,35 @@ auto kcombinations(vector <vector<int>> A, int k) {
     }
 
     return seconds;
+}
+
+template<typename T>
+auto toEigen(vector <T> A, int n) {
+
+    Eigen::MatrixXf eigen(n, n);
+
+    int k = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            eigen(i, j) = A[k++];
+        }
+    }
+
+    return eigen;
+}
+
+template<typename T>
+auto buildMatrix(vector <T> selection) {
+    vector <T> square;
+    int N = selection.size();
+
+    int i = 0;
+    sort(selection.begin(), selection.end());
+    while (i < N) {
+        square.insert(square.end(), selection.begin(), selection.end());
+        i++;
+        next_permutation(selection.begin(), selection.end());
+    }
+
+    return toEigen(square, N);
 }
--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #define CATCH_CONFIG_MAIN
-
 #include "catch.hpp"
 #include "library.cpp"
+#include <future>
+
+using namespace Eigen;
 
 TEST_CASE("7 choose 4 and 4 choose 3 is 140") {
     REQUIRE(choice(7, 4) * choice(4, 3) == 140);
@@ -51,3 +53,55 @@ SCENARIO("7 choose 4 then 4 choose 3") {
         }
     }
 }
+
+TEST_CASE("Build Square Matrix") {
+    vector<float> numbers = {1, 2, 3, 4, 5, 6, 7};
+    auto matrix = buildMatrix(numbers);
+
+    cout << matrix;
+}
+
+TEST_CASE("To Eigen") {
+    vector<float> numbers = {1, 2, 1, 2, 1, 0, -1, 1, 2};
+    auto eigen = toEigen(numbers, 3);
+    cout << eigen << endl << endl;
+    cout << eigen.inverse() << endl;
+}
+
+
+SCENARIO("7 choose 4 then 4 choose 3 the permute") {
+
+    GIVEN("I have seven numbers") {
+        vector<float> numbers = {1, 2, 1, 3, 1, 0, -1};
+
+        WHEN("I choose 4 from 7 then 3 from 4 numbers") {
+            auto combination = kcombinations(combinations(numbers, 4), 3);
+
+            THEN("I have 140 combinations") {
+
+                REQUIRE(combination.size() == 140);
+
+
+                for (auto c : combination) {
+                    auto mat = buildMatrix(c);
+
+                    if (mat.determinant() == 0)
+                        continue;
+
+                    cout << "Combinations:" << endl;
+                    for (auto e : c)
+                        cout << e << " ";
+                    cout << endl << endl;
+
+                    cout << "Matrix:" << endl;
+                    cout << mat << endl << endl;
+
+                    cout << "Inverse:" << endl;
+                    cout << mat.inverse() << endl << endl;
+                }
+
+            }
+        }
+    }
+}
+
